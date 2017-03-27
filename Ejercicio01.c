d4dbafe4e4b1b1481b21d8edfdc5ec310b268854
Vikkerissen <Vikkerissen32@gmail.com>
2017-03-26 22:44:30 -0300

    Add files via upload

---
+++ Ejercicio01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+int cant, val, sueldo;
+
+printf("\n1)Ingrese la cantidad de horas trabajadas: ");
+scanf("%d", &cant);
+
+printf("\n2)Ingrese el valor por hora del empleado: $");
+scanf("%d", &val);
+
+sueldo = cant*val;
+
+printf("\nEl sueldo del empleado es de: $%d", sueldo);
+
+getch();
+
+}
