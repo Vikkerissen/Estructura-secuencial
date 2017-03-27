51f135ade5f9401c60942aa9adbcc961890dd41d
Vikkerissen <Vikkerissen32@gmail.com>
2017-03-26 22:53:18 -0300

    Update Ejercicio05.c

--- Ejercicio05.c
+++ Ejercicio05.c
@@ -1,3 +1,4 @@
+//5-) Se ingresa un nro. entero que representa una fecha con formato (ddmmaa). Se pide transformarlo a un nro. con formato (aammdd).
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,7 +13,7 @@ dd = fecha/10000; //31 y resto 1215
 res = fecha%10000;
 mm = res/100; //12
 aa = res%100; //15
-fecha2 = aa*10000+mm*100+dd*1; //Se puede evitar esto así: printf("AAMMDD: %d%d%d", aa, mm, dd);
+fecha2 = aa*10000+mm*100+dd*1; //Se puede evitar esto asÃ­: printf("AAMMDD: %d%d%d", aa, mm, dd);
 
 printf("\nLa fecha en formato AAMMDD es: %d", fecha2);
 
