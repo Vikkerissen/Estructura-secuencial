207835e45ac65b1bffd87e0b6a2d0da37ed13d4a
Vikkerissen <Vikkerissen32@gmail.com>
2017-03-26 22:52:45 -0300

    Update Ejercicio04.c

--- Ejercicio04.c
+++ Ejercicio04.c
@@ -1,3 +1,4 @@
+//4-) Se ingresa un numero entero de 3 cifras. Descomponerlo en unidad , decena y centena.
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,10 +9,10 @@ int num, res, u, d, c;
 printf("\nIngrese un numero entero de 3 cifras: "); //Se prueba con 375
 scanf("%d", &num);
 
-c = num/100; //3,75 (el 3 ser· la centena y el 75 el resto)
+c = num/100; //3,75 (el 3 ser√° la centena y el 75 el resto)
 res = num%100; // 3,75 (nos quedamos con el resto (75))
-d = res/10; // 7,5 (el 7 ser· la decena, el resto ahora es 5)
-u = res%10; // 7,5 (el 5 ser· la unidad)
+d = res/10; // 7,5 (el 7 ser√° la decena, el resto ahora es 5)
+u = res%10; // 7,5 (el 5 ser√° la unidad)
 
 printf("\nLa centena es: %d", c);
 printf("\nLa decena es: %d", d);
