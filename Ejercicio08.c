4b576fe5ec91a4a491cb4c4090e4b941f98478d0
Vikkerissen <Vikkerissen32@gmail.com>
2017-03-26 22:58:03 -0300

    Update Ejercicio08.c

--- Ejercicio08.c
+++ Ejercicio08.c
@@ -1,3 +1,5 @@
+/*8-) Conociendo la cantidad de tarros de pintura que existe en un deposito de una pinturería y sabiendo que el 50% son tarros de 1Lt.,
+el 30% tarros de 4Lts. y el resto tarros de 20Lts. Determinar la cantidad de tarros de 1Lt. , 4Lts. Y 20 Lts. */
 #include <stdio.h>
 #include <stdlib.h>
 
